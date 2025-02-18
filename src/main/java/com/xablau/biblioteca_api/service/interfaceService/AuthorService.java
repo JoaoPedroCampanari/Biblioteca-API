fc1b69d8749ce3765e1d35010e11bf50741a5b2c
JoaoPedroCampanari <joaofcampanari@gmail.com>
2025-02-18 15:39:36 -0300

    author interface implemented

--- src/main/java/com/xablau/biblioteca_api/service/interfaceService/AuthorService.java
+++ src/main/java/com/xablau/biblioteca_api/service/interfaceService/AuthorService.java
@@ -2,16 +2,22 @@ package com.xablau.biblioteca_api.service.interfaceService;
 
 import com.xablau.biblioteca_api.dto.authorDtos.AuthorDto;
 import com.xablau.biblioteca_api.model.Author;
+import com.xablau.biblioteca_api.model.Book;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface AuthorService {
 
-    Author getById(AuthorDto authorDto);
+    Author findById(AuthorDto authorDto);
 
-    List<Author> getAll();
+    List<Author> findAll();
 
-    Author updateById(UUID id, AuthorDto authorDto);
+    Author update(UUID id, AuthorDto authorDto);
 
+    Author save(AuthorDto authorDto);
+
+    void deleteById(UUID id);
+
+    List<Book> findAllBooksAuthor(UUID id);
 }
