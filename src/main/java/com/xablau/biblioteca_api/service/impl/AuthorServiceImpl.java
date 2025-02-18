9cfc493d27234f028aab6bb67a8a401b8e9bc3c0
JoaoPedroCampanari <joaofcampanari@gmail.com>
2025-02-18 16:11:06 -0300

    docker compose implemented

---
+++ src/main/java/com/xablau/biblioteca_api/service/impl/AuthorServiceImpl.java
@@ -0,0 +1,51 @@
+package com.xablau.biblioteca_api.service.impl;
+
+import com.xablau.biblioteca_api.dto.authorDtos.AuthorDto;
+import com.xablau.biblioteca_api.model.Author;
+import com.xablau.biblioteca_api.model.Book;
+import com.xablau.biblioteca_api.repository.AuthorRepository;
+import com.xablau.biblioteca_api.service.interfaceService.AuthorService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class AuthorServiceImpl implements AuthorService {
+
+    public AuthorServiceImpl(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    private final AuthorRepository authorRepository;
+
+    @Override
+    public Author findById(AuthorDto authorDto) {
+        return null;
+    }
+
+    @Override
+    public List<Author> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public Author update(UUID id, AuthorDto authorDto) {
+        return null;
+    }
+
+    @Override
+    public Author save(AuthorDto authorDto) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(UUID id) {
+
+    }
+
+    @Override
+    public List<Book> findAllBooksAuthor(UUID id) {
+        return List.of();
+    }
+}
